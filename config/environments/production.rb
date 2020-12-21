0c8d72d685c03a31cea43e7af580323919048738
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-12-21 10:39:23 +0800

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # replace localhost with actual URL
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'quiet-falls-97147.herokuapp.com', protocol: 'https' }
 end
